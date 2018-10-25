a7eb952ab20eea69d4de154e462a8b41ac23753f
bradwbradw <bradwbradw@xyz.gs>
2018-10-24 17:14:25 -0700

    new test for multiple collections

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
+const _ = require('lodash');
+
+
+const mongoModule = require('.././mongo.js');
+
+
 let assert = require('assert');
 /*
 
@@ -15,24 +21,24 @@ let db;
 let dbName = 'test-db';
 
 let testDocument = {
-  id:22,
-  person:"jesss"
+  id: 22,
+  person: "jesss"
 };
 
 let options = {
   url: 'mongodb://localhost',
   collection: 'default-collection',
-  indexes:[],
-  db:dbName
+  indexes: [],
+  db: dbName
 };
 
 before(() => {
-  db = require('.././mongo.js')(options);
+  db = mongoModule(options);
 });
 
-describe('db operations', function() {
+describe('db operations', () => {
 
-  describe('inserting one document', function() {
+  describe('inserting one document', () => {
 
     before(done => {
 
@@ -50,21 +56,24 @@ describe('db operations', function() {
 
     it('upsert should update the doc', done => {
 
-      db.upsert({id:testDocument.id, person: "davey"})
-        .then(()=>{
+      db.upsert({
+        id: testDocument.id,
+        person: "davey"
+      })
+        .then(() => {
           return db.getById(testDocument.id);
         })
         .then(result => {
           assert.equal(result.person, "davey");
         })
-        .then(()=>{
+        .then(() => {
           return db.count()
         })
         .then(count => {
           assert.equal(count, 1);
           return db.upsert(testDocument);
         })
-        .then(()=>{
+        .then(() => {
 
           done()
         })
@@ -73,17 +82,85 @@ describe('db operations', function() {
 
   });
 
+  describe('second collection', () => {
+    let db2;
+    let db;
+
+    before((done) => {
+      dropDB()
+        .then(() => {
+
+          db = new mongoModule({
+            url: 'mongodb://localhost',
+            collection: 'collection',
+            indexes: [],
+            db: dbName
+          });
+
+          db2 = new mongoModule({
+            url: 'mongodb://localhost',
+            collection: 'collection2',
+            indexes: [],
+            db: dbName
+          });
+          done();
+        });
+    });
+
+    it.only('should insert new record into new collection', (done) => {
+      let err;
+
+      db.upsert({
+        id: 22,
+        person: "tuck"
+      })
+        .then(() => {
+          return db2.upsert({
+            id: 33,
+            person: "cal"
+          });
+        })
+        .then(db.get)
+        .then(results => {
+          assert.equal(_.size(results), 1);
+          console.log('11111')
+        })
+        .then(db2.get)
+        .then(results => {
+          console.log('33333')
+          assert.equal(_.size(results), 1);
+        })
+        .catch(e => {
+          console.log('44444')
+          err = e;
+          return Promise.resolve();
+        })
+        .then(() => {
+          console.log('5555555')
+          return dropDB();
+        })
+        .then(() => {
+          console.log('666666')
+          done(err)
+        });
+    });
+  })
+
 });
 
-after((done) =>  {
-  // runs after all tests in this block
-  let MongoClient = require('mongodb').MongoClient;
+after((done) => {
 
-  MongoClient.connect(options.url, {useNewUrlParser:true })
-    .then(mongoConnection => {
-      return mongoConnection.db(options.db).dropDatabase();
-    })
+  dropDB()
     .then(() => {
       done()
     });
 });
+
+function dropDB() {
+
+  return require('mongodb').MongoClient.connect(options.url, {useNewUrlParser: true})
+    .then(mongoConnection => {
+      return mongoConnection.db(options.db).dropDatabase();
+    })
+
+}
